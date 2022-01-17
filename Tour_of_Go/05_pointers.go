99b9fc84f6eae2ce070be6e34e6914e1ae903c57
Shresht Srivastav <Shresht7@outlook.com>
2022-01-17 13:06:26 +0530

    Pointers

---
+++ Tour_of_Go/05_pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, j := 42, 270
+
+	//	A pointer holds the memory address of a value
+
+	p := &i            //	p points to i. The `&` operator generates a pointer to its operand (var p *int)
+	fmt.Println(p, *p) //	`*` operator denotes the pointer's underlying value. reads i through the pointer p
+	*p = 21            //	Set i through the pointer p
+
+	fmt.Println(i)
+
+	//	This is known as 'dereferencing' or 'indirecting'
+	//	Unlike C, Go has no pointer arithmetic
+
+	p = &j
+	*p = *p / 37
+	fmt.Println(j)
+}
