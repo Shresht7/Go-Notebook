c09183c0d24297cb3af43cc77b1b84f81e264aff
Shresht Srivastav <Shresht7@outlook.com>
2022-01-17 12:38:51 +0530

    Refactor helloWorld.go

--- Tour_of_Go/00_helloWorld.go
+++ Tour_of_Go/00_helloWorld.go
@@ -1,11 +1,11 @@
 //	A package clause starts every source file
 /*
- *	Every go program is made up of packages.
- *	Main is a special name declaring this source file
- *	an executable rather than a library.
- *	Executables start running in the main package.
- * 	This was a multi-line comment btw
- */
+	Every go program is made up of packages.
+	Main is a special name declaring this source file
+	an executable rather than a library.
+	Executables start running in the main package.
+ 	This was a multi-line comment btw
+*/
 package main
 
 //	Import declarations declare library packages used in this source file
@@ -14,8 +14,8 @@ import (
 )
 
 //	Functions are defined using the `func` keyword
-//	Main is a special function determining the entry-point of the application
+//	`main` is a special function determining the entry-point of the application
 func main() {
-	//	Prinntln from the fmt package is used to output to stdout
+	//	Println from the fmt package is used to output to stdout
 	fmt.Println("Hello World!")
 }
