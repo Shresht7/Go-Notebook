dd38f267f2aae676b1c7fa8bf3f79867c38caf90
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-01-18 21:48:51 +0530

    Add stringers

---
+++ Tour_of_Go/12_stringers.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)\n", p.Name, p.age)
+}
+
+func main() {
+	a := Person{"Arthur Dent", 42}
+	b := Person{"Zaphod Beeblebrox", 9001}
+	fmt.Println(a, b)
+
+	exercise()
+}
+
+type IPAddr [4]byte
+
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+}
+
+func exercise() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+
+}
