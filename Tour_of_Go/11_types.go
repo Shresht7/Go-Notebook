abd4f877c30558e57b1e5e1bf2f4fb062aeb460b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-01-18 21:43:47 +0530

    Add types

---
+++ Tour_of_Go/11_types.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	//	To test whether an interface value holds a specific type,
+	//	a type assertion can return two values (the value itself, and a boolean to report whether the assertion succeeded)
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	typeSwitch()
+}
+
+func do(i interface{}) {
+	//	A type switch is a contstruct that permits several type assertions in series
+	//	A type-switch is like a regular switch statement, but the cases in a type-switch specify types.
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
+}
+
+func typeSwitch() {
+	do(21)
+	do("hello")
+	do(true)
+}
