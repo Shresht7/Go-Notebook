2be54a9e0f8555bf85770c9efde776fdcb26e8b5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-01-20 23:46:15 +0530

    Add channels

---
+++ 00_Tour_of_Go/16_goroutines.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//	A Goroutine is a lightweight thread managed by the Go runtime.
+//	`go f(x, y, z)` - starts a new goroutine running `f(x, y, z)`
+//	The evaluation of `f, x, y, z` happens in the current goroutine
+//	and the execution happens in the new goroutine.
+
+//	Goroutines run in the same address space, so access to shared memory must be synchronized.
+//	The sync package provides useful primitives, although you won't need them much in Go as there are other primitives.
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	go say("world")
+	say("hello")
+}
---
+++ 00_Tour_of_Go/17_channels.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+//	Channels are typed conduit through which you can send and receive values with the channel operator <-
+//	```
+//	ch <- y	//	Send y to channel ch
+//	x := <- ch	//	Receive from ch, and assign to x
+//	```
+//	The data flows in direction of the arrow
+
+//	Like maps and slices, channels must be created before use
+//	`ch := make(chan int)`
+
+//	By default, send and receive block until the other side is ready.
+//	This allows goroutines to synchronize without explicit locks or condition variables.
+
+//	This example code sums the numbers in a slice, distributing the work between two goroutines
+//	Once both goroutines have completed their computation, it calculates the final result.
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum //	Send sum to c
+}
+
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c //	Received from c
+
+	fmt.Println(x, y)
+}
