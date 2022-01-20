fa0fd21b61fbfb069411bc351028433978598e36
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-01-20 23:29:47 +0530

    Update errors and readers

--- 00_Tour_of_Go/13_errors.go
+++ 00_Tour_of_Go/13_errors.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+//	Go programs express errors with `error` values
+//	The `error` type is a built-in interface similar to `fmt.Stringer`
+//	```
+//	type error interface {
+//		Error() string
+//	}
+//	```
+
+//	As with fmt.Stringer, the fmt package looks for error interface when printing values.
+
 type MyError struct {
 	When time.Time
 	What string
@@ -22,6 +32,8 @@ func run() error {
 }
 
 func main() {
+	//	Functions often return an `error` value, and calling code should handle errors by testing whether the error equals `nil`
+	//	A `nil` error denotes success, a non-nil error denotes failure
 	if err := run(); err != nil {
 		fmt.Println(err)
 	}
