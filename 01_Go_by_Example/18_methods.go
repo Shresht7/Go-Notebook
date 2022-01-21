cf54c67223b38cc012716c0c53b6de247659d8e6
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-01-21 16:16:36 +0530

    Add pointer, structs, methods, interface examples

---
+++ 01_Go_by_Example/18_methods.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+//	Go supports methods defined on struct types
+
+type rect struct {
+	width, height int
+}
+
+//	This area method has a receiver type of *rect
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+//	Methods can be defined for either pointer or value receiver types.
+func (r rect) perimeter() int {
+	return 2*r.width + 2*r.height
+}
+
+func main() {
+
+	r := rect{width: 10, height: 5}
+
+	fmt.Println("area:", r.area())
+	fmt.Println("perimeter:", r.perimeter())
+
+	rp := &r
+	fmt.Println("area:", rp.area())
+	fmt.Println("perimeter:", rp.perimeter())
+
+	//	Go automatically handles conversion beteween values and pointers for method calls.
+	//	You may want to use a pointer receiver type to avoid copying on method calls or to allow the
+	//	method to mutate the receiving struct
+}
