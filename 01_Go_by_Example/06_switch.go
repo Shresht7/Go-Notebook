d989790e0ffda651485a2240a24d2121bb128632
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-01-21 14:58:03 +0530

    Add conditionals, arrays, slices and maps to examples

---
+++ 01_Go_by_Example/06_switch.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	//	Basic switch
+	i := 2
+	fmt.Println("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+	//	Note default case is options.
+	//	Also not there is no fallthrough cases in Go. First match and exit.
+
+	//	You can use commas to separate multiple expressions in the same case statement
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	//	Switch statement without and expression is an alternate way to express if/else logic (true is implied)
+	//	Case expressions can be non-constants
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	//	A typed switch compares types instead of values. You can use this to discover
+	//	the type of an interface value.
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know the type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
