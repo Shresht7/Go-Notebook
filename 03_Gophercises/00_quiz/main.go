725e052e18ed9b59d2178712c5bd31691c48937a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-05 12:21:02 +0530

    Add timer to gophercises quiz

--- 03_Gophercises/00_quiz/main.go
+++ 03_Gophercises/00_quiz/main.go
@@ -9,24 +9,24 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
-
-type Score struct {
-	correct int8
-	total   int8
-}
 
 func main() {
 
 	//	Get fileName
 	fileName := flag.String("fileName", "./problems.csv", "Problem Set")
+	timeLimit := flag.Int("time-limit", 20, "The Time Limit")
 	flag.Parse()
 
 	//	Instantiate scorecard
-	scorecard := Score{}
+	correct := 0
 
 	//	Read CSV file
 	data := readCSVFile(*fileName)
+
+	//	Create timer channel
+	timer := time.NewTimer(time.Duration(*timeLimit * int(time.Second)))
 
 	//	Play Quiz
 	for _, v := range data {
@@ -34,28 +34,35 @@ func main() {
 		//	Prompt question
 		fmt.Print(v[0], ": ")
 
-		//	Take input
-		var res string
-		fmt.Scanln(&res)
+		ansCh := make(chan string)
+		go func() {
+			//	Take input
+			var res string
+			fmt.Scanln(&res)
+			ansCh <- res
+		}()
 
-		//	Parse answer and user response as int
-		response, errRes := strconv.Atoi(strings.TrimSpace(res))
-		answer, errAns := strconv.Atoi(v[1])
+		select {
+		case <-timer.C:
+			fmt.Println("\nScored ", correct, " out of ", len(data))
+			return
+		case res := <-ansCh:
+			//	Parse answer and user response as int
+			response, errRes := strconv.Atoi(res)
+			answer, errAns := strconv.Atoi(v[1])
 
-		//	If no errors while conversion
-		if errRes == nil && errAns == nil {
-			//	...and user response matches answer
-			if response == answer {
-				scorecard.correct++ //	Increment score
+			//	If no errors while conversion
+			if errRes == nil && errAns == nil {
+				//	...and user response matches answer
+				if response == answer {
+					correct++ //	Increment score
+				}
 			}
 		}
-
-		//	Increment total questions attempted
-		scorecard.total++
 	}
 
 	//	Show scorecard
-	fmt.Println("\nScored ", scorecard.correct, " out of ", scorecard.total)
+	fmt.Println("\nScored ", correct, " out of ", len(data))
 }
 
 func readCSVFile(fileName string) [][]string {
